f891971a819122dd252c39fb1da00b6d69ea59ef
Ben <benjo1996@live.co.uk>
2021-06-01 15:49:34 +0100

    First commit

---
+++ msgGenerator.js
@@ -0,0 +1,7 @@
+//The following are arrays in which the different random values will come from
+
+var countries = ['England', 'Italy', 'France'];
+var capitals = ['London', 'Rome', 'Paris'];
+var leaders = ['Boris Johnson', 'Sergio Mattarella', 'Emmanuel Macron'];
+
+console.log("Hello");
