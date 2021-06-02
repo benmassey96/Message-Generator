6f075701f4d84c6297d3f3c2a755ae1130f97c8f
Ben <benjo1996@live.co.uk>
2021-06-02 10:31:04 +0100

    Ready to put message together

--- msgGenerator.js
+++ msgGenerator.js
@@ -4,16 +4,59 @@ const countries = ['England', 'Italy', 'France'];
 const capitals = ['London', 'Rome', 'Paris'];
 const leaders = ['Boris Johnson', 'Sergio Mattarella', 'Emmanuel Macron'];
 
+//Select a random index 
 let randomCountry = Math.floor(Math.random() * countries.length);
 let randomCapital = Math.floor(Math.random() * capitals.length);
 let randomLeader = Math.floor(Math.random() * leaders.length);
+let country;
+let capital;
+let leader;
+
+
+switch (randomCountry) {
+    case 0:
+        country = 'England';
+        break;
+    case 1:
+        country = 'Italy';
+        break;
+    case 2:
+        country = 'France';
+};
+
+switch (randomCapital) {
+    case 0:
+        capital = 'London';
+        break;
+    case 1:
+        capital = 'Rome';
+        break;
+    case 2:
+        capital = 'Paris';
+        break;
+};
+
+switch (randomLeader) {
+    case 0:
+        leader = 'Boris Johnson';
+        break;
+    case 1:
+        leader = 'Sergio Mattarella';
+        break;
+    case 2:
+        leader = 'Emmanuel Macron';
+        break;
+}
 
 //Testing arrays print to console
-console.log(countries);
-console.log(capitals);
-console.log(leaders);
+//console.log(countries);
+//console.log(capitals);
+//console.log(leaders);
 
-//Testing random indecies from arrays
-console.log(randomCountry);
-console.log(randomCapital);
-console.log(randomLeader);
+//Testing random indices from arrays
+//console.log(randomCountry);
+console.log(country);
+//console.log(randomCapital);
+console.log(capital);
+//console.log(randomLeader);
+console.log(leader);
